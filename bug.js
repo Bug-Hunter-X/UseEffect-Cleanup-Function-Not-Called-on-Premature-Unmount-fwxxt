03bc677a459c78a9f75b134a8c65ef267c6dbd7c
BugHunter-X <askzenai@gmail.com>
2025-01-20 01:34:12 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,21 @@
+```javascript
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // This effect runs only once on mount
+    console.log('Mounted!');
+    return () => {
+      // Cleanup function
+      console.log('Unmounted!');
+    };
+  }, []);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+}
+```
